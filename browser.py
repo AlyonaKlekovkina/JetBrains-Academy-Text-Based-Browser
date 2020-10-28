6c8a1a01e5221d5d2d665486e2646b611cfe3187
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-10-28 15:43:30 +0300

    Stage #2: Tabs

--- browser.py
+++ browser.py
@@ -1,3 +1,6 @@
+import os
+import os.path
+import sys
 
 nytimes_com = '''
 This New Liquid Is Magnetic, and Mesmerizing
@@ -33,14 +36,58 @@ Twitter and Square Chief Executive Officer Jack Dorsey
  addressed Apple Inc. employees at the iPhone maker’s headquarters
  Tuesday, a signal of the strong ties between the Silicon Valley giants.
 '''
-
 # write your code here
+args = sys.argv
+dir_name = args[1]
+tabs_list = []
 
+
+def save_file(name):
+    n = name.split('.')
+    n = n[0]
+    the_name = os.path.join(dir_name, "{}".format(n))
+    with open(the_name, "w") as f:
+        if n == 'bloomberg':
+            f.write(bloomberg_com)
+            options.append(n)
+        else:
+            f.write(nytimes_com)
+            options.append(n)
+
+
+def read_file(name):
+    the_name = os.path.join(dir_name, name)
+    with open(the_name, 'r', encoding='utf-8') as f:
+        for line in f:
+            print(line.strip())
+
+
+def create_directory():
+    try:
+        os.mkdir(dir_name)
+        tabs_list.append(dir_name)
+    except FileExistsError:
+        tabs_list.append(dir_name)
+
+
+if len(args) > 1:
+    create_directory()
+
+
+options = ['bloomberg.com', 'nytimes.com', 'exit']
 while True:
     inp = input()
-    if inp == 'bloomberg.com':
+    if inp not in options:
+        print("Error: Incorrect URL")
+    elif inp == 'bloomberg.com':
+        save_file('bloomberg.com')
         print(bloomberg_com)
     elif inp == 'nytimes.com':
+        save_file('nytimes.com')
         print(nytimes_com)
+    elif inp == 'bloomberg' and inp in options and len(tabs_list) >= 1:
+        read_file('bloomberg')
+    elif inp == 'nytimes' and inp in options and len(tabs_list) >= 1:
+        read_file('nytimes')
     elif inp == 'exit':
         break
