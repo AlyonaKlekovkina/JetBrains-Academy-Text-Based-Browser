8ce1d514a6b22d50f289eb2779ae32d4b427ee58
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-10-29 14:18:10 +0300

    Stage #3: Hotkeys

--- browser.py
+++ browser.py
@@ -1,3 +1,6 @@
+import os
+import os.path
+import sys
 
 nytimes_com = '''
 This New Liquid Is Magnetic, and Mesmerizing
@@ -33,14 +36,69 @@ Twitter and Square Chief Executive Officer Jack Dorsey
  addressed Apple Inc. employees at the iPhone maker’s headquarters
  Tuesday, a signal of the strong ties between the Silicon Valley giants.
 '''
-
 # write your code here
+args = sys.argv
+dir_name = args[1]
+tabs_list = []
 
+
+def save_file(name):
+    n = name.split('.')
+    n = n[0]
+    the_name = os.path.join(dir_name, "{}".format(n))
+    with open(the_name, "w") as f:
+        if n == 'bloomberg':
+            f.write(bloomberg_com)
+        else:
+            f.write(nytimes_com)
+
+
+def read_file(name):
+    the_name = os.path.join(dir_name, name)
+    with open(the_name, 'r', encoding='utf-8') as f:
+        for line in f:
+            print(line.strip())
+
+
+def create_directory():
+    try:
+        os.mkdir(dir_name)
+        tabs_list.append(dir_name)
+    except FileExistsError:
+        tabs_list.append(dir_name)
+
+
+if len(args) > 1:
+    create_directory()
+
+
+options = ['bloomberg.com', 'nytimes.com', 'exit', 'back']
+saved_files = []
 while True:
     inp = input()
-    if inp == 'bloomberg.com':
+    if inp not in options:
+        print("Error: Incorrect URL")
+    elif inp == 'bloomberg.com':
+        save_file('bloomberg.com')
+        saved_files.append('bloomberg')
         print(bloomberg_com)
     elif inp == 'nytimes.com':
+        save_file('nytimes.com')
+        saved_files.append('nytimes')
         print(nytimes_com)
+    elif inp == 'bloomberg' and inp in saved_files:
+        read_file('bloomberg')
+    elif inp == 'nytimes' and inp in saved_files:
+        read_file('nytimes')
+    elif inp == 'back':
+        if len(saved_files) >= 2:
+            current = saved_files.pop()
+            previous = saved_files.pop()
+            if previous == 'bloomberg':
+                print(bloomberg_com)
+            elif previous == 'nytimes':
+                print(nytimes_com)
+        elif len(saved_files) < 2:
+            continue
     elif inp == 'exit':
         break
